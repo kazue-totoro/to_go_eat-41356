8d1fd3907cdf6ae95bfdc04159199c5d9e18712e
kazue-totoro <kazue.kii.1214@gmail.com>
2024-09-09 16:01:58 +0900

    ビュー編集

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,20 @@
 class ShopsController < ApplicationController
   def index
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:image, :shop_category_id, :name, :address, :phone_number).merge(user_id: current_user.id)
   end
 end
