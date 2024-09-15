1a0af2da26363753593fa02607437a198f5ce1ed
kazue-totoro <kazue.kii.1214@gmail.com>
2024-09-15 14:34:29 +0900

    修正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,6 @@
 class Shop < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :shop_category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :visited, inclusion: { in: [true, false] }
 
   belongs_to :user
   has_one_attached :image
